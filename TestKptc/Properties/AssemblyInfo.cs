569ddfbcd75eb76d15619b7583aa8a0e36ece337
TerminalNavi <156845332+Pavlova-Ole@users.noreply.github.com>
2024-05-06 09:30:49 +0300

    init: first commit

---
+++ TestKptc/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestKptc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestKptc")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f69b7bec-575b-4065-8756-f13969346596")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestKptc/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using KptcLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestKptc
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 3;
+            int b = Class1.LibraryTestMethod(1, 2);
+            Assert.AreEqual(a, b, "Сложение не работает");
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int a = -3;
+            int b = Class1.LibraryTestMethod(-1, -2);
+            Assert.AreEqual(a, b, "Сложение не работает");
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int a = -3;
+            int b = Class1.LibraryTestMethod(1, 3);
+            Assert.AreNotEqual(a, b, "Сложение не работает");
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int a = -1;
+            int b = Class1.LibraryTestMethod(1, -2);
+            Assert.AreEqual(a, b, "Сложение не работает");
+        }
+
+    }
+}
